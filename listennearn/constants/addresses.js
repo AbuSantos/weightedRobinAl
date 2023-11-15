c81d32658f40523fe0a6e6db177b25d7fbd5b1c0
Abusomwan Santos <69383406+AbuSantos@users.noreply.github.com>
2023-11-15 22:35:56 +0100

    Delete listennearn directory

--- listennearn/constants/addresses.js
+++
@@ -1,4 +0,0 @@
-export const listenToEarn_address =
-  "0xAC7ED848343b0A1838A16Bd1e9e94686979e5dae";
-export const myToken_address = "0xFa0EdF5C9DbfD5156a01E6BDc1b220Ca338AD4a2";
-export const nft_address = "0xC87df6a82Ff508C3d3cA12c3b7ef9f04a03BA1bD";
